ff18bb82d2c56b303ca38c114bb344673b0fbb0f
voluble-bug-0219 <montensia1@outlook.com>
2019-05-14 23:46:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+katz_deli = []
+
+def line customers
+  if customers.count == 0
+    puts "The line is currently empty."
+  else
+    line = "The line is currently:"
+    customers.each_with_index do |customer, index|
+      line << " #{index + 1}. #{customer}"
+    end
+    puts line
+  end
+  
+end
+
+def take_a_number
+  
+end
+
+def now_serving
+  
+end
+
