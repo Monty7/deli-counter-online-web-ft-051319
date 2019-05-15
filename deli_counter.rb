70805ec2e574aae4b409d49e1b4d8c966680d78e
voluble-bug-0219 <montensia1@outlook.com>
2019-05-15 00:18:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,7 @@ def take_a_number (customers, name)
   customers << name
   
   customers.each_with_index do |customer, index|
-    puts "Welcome #{customer}. You are number #{index + 1} in line."
+    puts "Welcome, #{customer}. You are number #{index + 1} in line."
   end
 end
 
